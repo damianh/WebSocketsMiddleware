1652363996a2d8b176a4897b55a2fb1a4e4a3dd9
Damian Hickey <dhickey@gmail.com>
2014-06-11 22:44:02 +0200

    more appveyor stuff

--- build.ps1
+++ build.ps1
@@ -1,11 +1,13 @@
 param(
 	[int]$buildNumber = 0
 	)
-Test-Path Env:\APPVEYOR_BUILD_NUMBER
+
 if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
 	$buildNumber = [int]$Env:APPVEYOR_BUILD_NUMBER
 	Write-Host "Using APPVEYOR_BUILD_NUMBER"
 }
+
+"Build number $buildNumber"
 
 $packageConfigs = Get-ChildItem . -Recurse | where{$_.Name -eq "packages.config"}
 foreach($packageConfig in $packageConfigs){
