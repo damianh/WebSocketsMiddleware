8010a1fe5b65f677a23b0b97390f8fa54b51e53c
Damian Hickey <dhickey@gmail.com>
2014-06-11 11:53:28 +0200

    Shared assembly info

--- src/WebSocketsMiddleware/Properties/AssemblyInfo.cs
+++ src/WebSocketsMiddleware/Properties/AssemblyInfo.cs
@@ -1,13 +1,3 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("WebSocketsMiddleware")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebSocketsMiddleware")]
-[assembly: AssemblyCopyright("Copyright © Damian Hickey 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
