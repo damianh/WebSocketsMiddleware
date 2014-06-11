38604878d381af5ccaea745c400b24d66d3ccf95
Damian Hickey <dhickey@gmail.com>
2014-06-11 15:09:22 +0200

    Test to ensure that http and websocket can be hosted at same endpoint.

--- src/WebSocketsMiddleware.Tests/WebSocketsMiddlewareTests.cs
+++ src/WebSocketsMiddleware.Tests/WebSocketsMiddlewareTests.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq;
+    using System.Net.Http;
     using System.Net.WebSockets;
     using System.Text;
     using System.Threading;
@@ -19,18 +20,45 @@
         {
             using (WebApp.Start("http://localhost:5000/", app => app.UseWebSockets(WebSocketReverseMessage)))
             {
-                var clientWebSocket = new ClientWebSocket();
-                await clientWebSocket.ConnectAsync(new Uri("ws://localhost:5000/"), CancellationToken.None);
-                byte[] bytes = Encoding.UTF8.GetBytes("Hello");
-                await clientWebSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, false, CancellationToken.None);
+                string response = await SendWebsocketMessage("ws://localhost:5000/", "Hello");
 
-                var buffer = new byte[1024];
-                WebSocketReceiveResult result =
-                    await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                string s = Encoding.UTF8.GetString(buffer, 0, result.Count);
-
-                s.Should().Be("olleH");
+                response.Should().Be("olleH");
             }
+        }
+
+        [Fact]
+        public async Task Can_have_http_and_websocket_at_same_uri()
+        {
+            using (WebApp.Start("http://localhost:5000/",
+                app => app
+                    .UseWebSockets(WebSocketEcho)
+                    .Use(async (context, _)  =>
+                    {
+                        await context.Response.WriteAsync("http request");
+                    })))
+            {
+                string websocketResponse = await SendWebsocketMessage("ws://localhost:5000/", "ws request");
+                websocketResponse.Should().Be("ws request");
+
+                using (var httpclient = new HttpClient())
+                {
+                    var httpResponse = await httpclient.GetAsync("http://localhost:5000/");
+                    (await httpResponse.Content.ReadAsStringAsync()).Should().Be("http request");
+                }
+            }
+        }
+
+        private static async Task<string> SendWebsocketMessage(string uri, string message)
+        {
+            var clientWebSocket = new ClientWebSocket();
+            await clientWebSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            await clientWebSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, false, CancellationToken.None);
+
+            var buffer = new byte[1024];
+            WebSocketReceiveResult result =
+                await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            return Encoding.UTF8.GetString(buffer, 0, result.Count);
         }
 
         private static async Task WebSocketReverseMessage(IWebSocketContext webSocketContext)
@@ -43,6 +71,24 @@
                 byte[] reversedMessage = Encoding.UTF8.GetBytes(String.Concat(Encoding.UTF8.GetString(buffer, 0, received.Count).Reverse()));
                 await webSocketContext.Send(
                     new ArraySegment<byte>(reversedMessage, 0, reversedMessage.Length),
+                    received.MessageType,
+                    received.EndOfMessage);
+
+                received = await webSocketContext.Receive(new ArraySegment<byte>(buffer));
+            }
+
+            await webSocketContext.Close();
+        }
+
+        private static async Task WebSocketEcho(IWebSocketContext webSocketContext)
+        {
+            var buffer = new byte[1024];
+            IWebSocketReceiveResult received = await webSocketContext.Receive(new ArraySegment<byte>(buffer));
+
+            while (!webSocketContext.ClientClosed)
+            {
+                await webSocketContext.Send(
+                    new ArraySegment<byte>(buffer, 0, received.Count),
                     received.MessageType,
                     received.EndOfMessage);
 
